71541c5189e05d8e4515fd28f77b9267705acead
JosephAlexTG <98728675+JosephAlexTG@users.noreply.github.com>
2022-02-17 14:51:27 +0530

    🤐

--- Broadcast/broadcast.py
+++ Broadcast/broadcast.py
@@ -1,11 +1,3 @@
-
-from pyrogram import Client, filters
-import datetime
-import time
-from database.users_chats_db import db
-from info import ADMINS
-from utils import broadcast_messages
-import asyncio
         
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
 # https://t.me/GetTGLink/4178
